7d1391608325a055f0e29846d7c6ef56f30b9a05
EscapingGithub <andrewquach10@gmail.com>
2017-01-16 19:07:20 -0800

    Working V1

--- cogs/ctf.py
+++ cogs/ctf.py
@@ -11,69 +11,69 @@ class CTF:
         self.bot = bot
         
     @commands.command(description='Caesar Cipher')
-    async def caesar(shift:int, plaintext:str):
+    async def caesar(self, shift:int, plaintext:str):
         alphabet = string.ascii_lowercase
         shifted_alphabet = alphabet[shift%26:] + alphabet[:shift%26]
         table = str.maketrans(alphabet, shifted_alphabet)
-        await bot.say(plaintext.lower().translate(table))
+        await self.bot.say(plaintext.lower().translate(table))
 
     @commands.command()
-    async def add(*args:int):
-        await bot.say(sum(args))
+    async def add(self, *args:int):
+        await self.bot.say(sum(args))
 
     @commands.command()
-    async def addbin(*args:str):
+    async def addbin(self, *args:str):
         tmp = [int(k, 2) for k in args]
-        await bot.say(bin(sum(tmp)))
+        await self.bot.say(bin(sum(tmp)))
 
     @commands.command()
-    async def addhex(*args:str):
+    async def addhex(self, *args:str):
         tmp = [int(k, 16) for k in args]
-        await bot.say(hex(sum(tmp)))
+        await self.bot.say(hex(sum(tmp)))
 
     @commands.command()
-    async def md5(message:str):
-        await bot.say(hashlib.md5(message.encode()).hexdigest())
+    async def md5(self, message:str):
+        await self.bot.say(hashlib.md5(message.encode()).hexdigest())
 
     @commands.command()
-    async def sha1(message:str):
-        await bot.say(hashlib.sha1(message.encode()).hexdigest())
+    async def sha1(self, message:str):
+        await self.bot.say(hashlib.sha1(message.encode()).hexdigest())
 
     @commands.command()
-    async def sha256(message:str):
-        await bot.say(hashlib.sha256(message.encode()).hexdigest())
+    async def sha256(self, message:str):
+        await self.bot.say(hashlib.sha256(message.encode()).hexdigest())
 
     @commands.command()
-    async def bintohex(number:str):
-        await bot.say(hex(int(number, 2)))
+    async def bintohex(self, number:str):
+        await self.bot.say(hex(int(number, 2)))
 
     @commands.command()
-    async def bintodec(number:str):
-        await bot.say(int(number, 2))
+    async def bintodec(self, number:str):
+        await self.bot.say(int(number, 2))
 
     @commands.command()
-    async def dectohex(number:int):
-        await bot.say(hex(number))
+    async def dectohex(self, number:int):
+        await self.bot.say(hex(number))
 
     @commands.command()
-    async def dectobin(number:int):
-        await bot.say(bin(number))
+    async def dectobin(self, number:int):
+        await self.bot.say(bin(number))
 
     @commands.command()
-    async def hextodec(number:str):
-        await bot.say(int(number, 16))
+    async def hextodec(self, number:str):
+        await self.bot.say(int(number, 16))
 
     @commands.command()
-    async def hextobin(number:str):
-        await bot.say(bin(int(number, 16)))
+    async def hextobin(self, number:str):
+        await self.bot.say(bin(int(number, 16)))
 
     @commands.command()
-    async def base64(message:str):
-        await bot.say(base64.b64encode(message.encode('utf-8')).decode('utf-8'))
+    async def base64(self, message:str):
+        await self.bot.say(base64.b64encode(message.encode('utf-8')).decode('utf-8'))
 
     @commands.command()
-    async def base64d(message:str):
-        await bot.say(base64.b64decode(message.encode('utf-8')).decode('utf-8'))
+    async def base64d(self, message:str):
+        await self.bot.say(base64.b64decode(message.encode('utf-8')).decode('utf-8'))
 
 def setup(bot):
    bot.add_cog(CTF(bot))
--- cogs/misc.py
+++ cogs/misc.py
@@ -7,19 +7,19 @@ class Misc:
         self.bot = bot
 
     @commands.command()
-    async def killme():
-        await bot.say('Suicide Hotline: 1-800-273-8255')
+    async def killme(self):
+        await self.bot.say('Suicide Hotline: 1-800-273-8255')
 
     @commands.command()
-    async def documentation():
-        await bot.say('''Is there any documentation for this? 
+    async def documentation(self):
+        await self.bot.say('''Is there any documentation for this? 
         
     Not at the moment.
     ''')
 
     @commands.command()
-    async def tamir():
-        await bot.say('Feels bad man :(')
+    async def tamir(self):
+        await self.bot.say('Feels bad man :(')
 
 def setup(bot):
     bot.add_cog(Misc(bot))
